347691d92ddacf8b14532d7c4abd77b106b694dc
Sagar Bisht <itssagar11@gmail.com>
2023-03-08 16:33:15 +0530

    Hero part

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import {Suspense,useEffect,useState} from 'react'
+import { Canvas} from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF} from '@react-three/drei';
+// import CanvasLoader from '../Loader';
+
+const computer =useGLTF('./desktop_pc/scene.gltf');
 
 const Computers = () => {
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor='black'></hemisphereLight>
+      <pointLight intensity={1}/>
+      <primitive
+      object={computer.scene}
+      />
+    </mesh>
   )
 }
-
-export default Computers
+const ComputersCanvas=()=>{
+  return(
+    <Canvas
+    frameLoop='demand'
+    shadows
+    camera={{position:[20, 3, 5],fov:25}}
+    gl={{preserveDrawingBuffer:true}}
+    >
+<Suspense fallback={<CanvasLoader />}>
+  <OrbitControls enableZoom={true}  maxPolarAngle={Math.Pi / 2}  minPolarAngle={Math.PI / 2}/>
+  <Computers />
+</Suspense>
+<Preload all />
+    </Canvas>
+  )
+}
+export default ComputersCanvas
